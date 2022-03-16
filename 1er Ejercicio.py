2f5777f5e895daf2b4b533cb65fd9d4c4e9f3951
JVELASQUEZV13 <101685254+JVELASQUEZV13@users.noreply.github.com>
2022-03-16 14:47:34 -0400

    Add files via upload

---
+++ 1er Ejercicio.py
@@ -0,0 +1,8 @@
+Python 3.9.10 (tags/v3.9.10:f2f3f53, Jan 17 2022, 15:01:48) [MSC v.1929 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> var1 = "Hola Prog1, Soy"
+>>> var2 = "Jose Angel Velasquez Vargas"
+>>> 
+>>> print(var1,var2)
+Hola Prog1, Soy Jose Angel Velasquez Vargas
+>>> 
